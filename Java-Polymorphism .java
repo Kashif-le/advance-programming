38c4b809d5e4f4742039312b4dd98dab09d88de7
kashif <31399601+Kashif-le@users.noreply.github.com>
2021-04-11 03:32:32 -0700

    Add files via upload

---
+++ Java-Polymorphism .java
@@ -0,0 +1,27 @@
+public class Polymorphism{
+
+     public static void main(String []args){
+         
+         
+     class Animal {
+  public void animalSound() {
+    System.out.println("The animal makes a sound");
+  }
+}
+
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Dog extends Animal {
+  public void animalSound() {
+    System.out.println("The dog says: bow wow");
+  }
+}
+    
+}
+
+     
+}
